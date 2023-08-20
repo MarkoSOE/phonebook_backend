846b566c8c48bac30cf40f9202d18f31c55f5166
Marko Alhamadani <Markoalhamadani@gmail.com>
2023-08-20 19:20:16 -0400

    latest

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
 	origin: "*",
-	credentials: true, //access-control-allow-credentials:true
+	credentials: true,
 	optionSuccessStatus: 200,
 };
 
